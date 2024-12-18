e3d4d8e404c96b7d4babb983d3a8e2706c44025a
teka <teka.valiev@gmail.com>
2024-12-18 19:07:29 +0900

    local ip added

--- src/assets/env.js
+++ src/assets/env.js
@@ -6,5 +6,5 @@
     window["env"]["APP_BASE_URL"] = "apiUrl: http://localhost:8080/api",
     window["env"]["APP_SHIPPING_URL"] = 'http://localhost:9090/api"';
     window["env"]["APP_MAP_API_KEY"] = '';
-    window["env"]["APP_DEFAULT_LANGUAGE"] = 'en';
+    window["env"]["APP_DEFAULT_LANGUAGE"] = 'ko';
   })(this);
